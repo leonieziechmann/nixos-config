5270aec62236c84ae305b1afcbf4db0f517249e0
= <>
2025-12-13 00:57:57 +0100

    changed default git user

--- home/git.nix
+++ home/git.nix
@@ -1,40 +1,39 @@
 { lib, config, ... }: {
-  options = {
-    git.setCredentials = lib.mkEnableOption "username and email";
-    git.addAliases = lib.mkEnableOption "useful git aliases";
-  };
+	options = {
+		git.setCredentials = lib.mkEnableOption "username and email";
+		git.addAliases = lib.mkEnableOption "useful git aliases";
+	};
 
+	config = lib.mkMerge [
+		(lib.mkIf config.git.setCredentials {
+		 programs.git = {
+		 userName = "Leonie Ziechmann";
+		 userEmail = "48921293+leonieziechmann@users.noreply.github.com";
+		 };
+		 })
 
-  config = lib.mkMerge [
-    (lib.mkIf config.git.setCredentials {
-      programs.git = {
-        userName = "Jakob Ziechmann";
-        userEmail = "48921293+Jakob-Ziechmann@users.noreply.github.com";
-      };
-    })
-    
-    (lib.mkIf config.git.addAliases {
-      home.shellAliases = {
-        gaa = "git add .";
-        gs = "git status";
-        gc = "git commit";
+		(lib.mkIf config.git.addAliases {
+			home.shellAliases = {
+				gaa = "git add .";
+				gs = "git status";
+				gc = "git commit";
 				gca = "git add . && git commit";
-      };
+			};
 
 			programs.git.aliases = {
 				graph = ''log --oneline --graph --decorate --all'';
 			};
-    })
+		})
 
-    {
-      programs.git.enable = true;
+		{
+			programs.git.enable = true;
 
-      git.setCredentials = lib.mkDefault true;
-      git.addAliases = lib.mkDefault true;
+			git.setCredentials = lib.mkDefault true;
+			git.addAliases = lib.mkDefault true;
 
 			programs.git.extraConfig = {
 				pull.rebase = true;
 			};
-    }
-  ];
+		}
+	];
 }
